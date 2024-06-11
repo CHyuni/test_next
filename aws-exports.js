107e99fa86f8c35383de3bd668ead57e01913e9b
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 16:59:40 +0900

    add

--- aws-exports.js
+++ aws-exports.js
@@ -1,7 +1,10 @@
-const awsmobile = {
-	"aws_region": process.env.DEFAULT_REGION,
-	"aws_s3_bucket": process.env.S3_BUCKET_NAME,
-	"aws_user_pool_id": process.env.USER_POOL_ID,
-	"aws_app_client_id": process.env.APP_CLIENT_ID,
-	"aws_identity_pool_id": process.env.IDENTITY_POOL_ID
+const awsconfig = {
+	"aws_project_region": process.env.DEFAULT_REGION,
+	"aws_user_files_s3_bucket": process.env.S3_BUCKET_NAME,
+	"aws_user_files_s3_bucket_region": process.env.DEFAULT_REGION,
+	"aws_user_pools_id": process.env.USER_POOL_ID,
+	"aws_user_pools_web_client_id": process.env.APP_CLIENT_ID,
+	"aws_cognito_identity_pool_id": process.env.IDENTITY_POOL_ID,
+	"aws_cognito_region": process.env.DEFAULT_REGION
 }
+export default awsconfig;

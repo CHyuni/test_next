418f82f5759c75e53a8ae52d7b6e6d080ef8605e
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 14:52:11 +0900

    add

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import { Inter } from "next/font/google";
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 const inter = Inter({ subsets: ["latin"] });
 

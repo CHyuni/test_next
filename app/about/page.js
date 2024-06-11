80bbda7165c24bc18a9d437f7158d0723519fe01
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 11:52:39 +0900

    environment test

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function envtest() {
   return (
     <div>
       <p>region: {process.env.DEFAULT_REGION}</p>
-      <p>bucket: {precoess.env.S3_BUCKET_NAME}</p>
+      {/* <p>bucket: {precoess.env.S3_BUCKET_NAME}</p> */}
       {/* <p>user_pool: {process.env.USER_POOL_ID}</p> */}
     </div>
   );

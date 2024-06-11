76b6989ed17952a9331c0547890ad69bdfb453fe
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 11:46:32 +0900

    environment test

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,6 @@ export default function Home() {
     <div>
       <p>region: {process.env.DEFAULT_REGION}</p>
       <p>bucket: {precoess.env.S3_BUCKET_NAME}</p>
-      <p>USER_POOL_ID</p>
       <p>user_pool: {process.env.USER_POOL_ID}</p>
     </div>
   );

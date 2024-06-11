20db0344213cd212caaa7376b9939fe2eee68f17
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 13:45:21 +0900

    environment test

--- app/about/page.js
+++ app/about/page.js
@@ -6,6 +6,8 @@ export default function envtest() {
       <p>region: {process.env.DEFAULT_REGION}</p>
       <p>bucket: {process.env.S3_BUCKET_NAME}</p>
       <p>user_pool: {process.env.USER_POOL_ID}</p>
+      <p>identity_pool: {process.env.IDENTITY_POOL_ID}</p>
+      <p>user_pool_client_id: {process.env.APP_CLIENT_ID}</p>
     </div>
   );
 }

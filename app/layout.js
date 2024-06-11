f5781a655fd13f185e724e9e7f1a932c7af26bc8
CHyuni <apple141410@nate.com>
2024-06-11 21:40:36 +0900

    add

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
-'use client';
-
-import { useEffect } from 'react';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from '../aws-exports';
 
-export default function Layout({ children }) {
-  useEffect(() => {
-    Amplify.configure(awsconfig);
-  }, []);
+Amplify.configure(awsconfig);
 
-  return <div>{children}</div>;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }

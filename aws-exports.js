26df71543ddbf7591d81086b08dd11b39fd93dd8
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-11 14:21:22 +0900

    environment test

--- aws-exports.js
+++ aws-exports.js
@@ -2,5 +2,6 @@ const awsmobile = {
 	"aws_region": process.env.DEFAULT_REGION,
 	"aws_s3_bucket": process.env.S3_BUCKET_NAME,
 	"aws_user_pool_id": process.env.USER_POOL_ID,
-	"aws_app_client_id": process.env.APP_CLIENT_ID
+	"aws_app_client_id": process.env.APP_CLIENT_ID,
+	"aws_identity_pool_id": process.env.IDENTITY_POOL_ID
 }

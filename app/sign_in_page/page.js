2d449061533f952ee44bf4e9eb87b68108777587
CHyuni <apple141410@nate.com>
2024-06-11 21:59:11 +0900

    add

--- app/sign_in_page/page.js
+++ app/sign_in_page/page.js
@@ -1,17 +1,20 @@
-'use client'
+'use client';
 
-import { Auth } from 'aws-amplify';
 import { useState } from 'react';
+import { signIn } from 'aws-amplify/auth';
 
 export default function SignIn() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
-  const signIn = async () => {
+  const handleSignIn = async () => {
     try {
-      const user = await Auth.signIn(username, password);
-      console.log('로그인 성공:', user);
+      const { isSignedIn, nextStep } = await signIn({
+        username,
+        password,
+      });
+      console.log('로그인 성공:', isSignedIn, nextStep);
       // 로그인 성공 시 처리
     } catch (error) {
       console.log('로그인 실패:', error);
@@ -23,19 +26,19 @@ export default function SignIn() {
     <div>
       <h1>로그인</h1>
       {error && <p>{error}</p>}
-      <input 
-        type="text" 
-        placeholder="사용자 이름" 
-        value={username} 
-        onChange={(e) => setUsername(e.target.value)} 
+      <input
+        type="text"
+        placeholder="사용자 이름"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <input 
-        type="password" 
-        placeholder="비밀번호" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={signIn}>로그인</button>
+      <button onClick={handleSignIn}>로그인</button>
     </div>
   );
 }

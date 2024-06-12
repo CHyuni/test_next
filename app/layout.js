c18ca338a157205bbfb66e55b047d5b9f730df1f
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-12 16:14:22 +0900

    add

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 // 'use client'
 
-import { useEffect } from 'react';
-import { Amplify } from 'aws-amplify';
-import awsconfig from '../aws-exports';
+// import { useEffect } from 'react';
+// import { Amplify } from 'aws-amplify';
+// import awsconfig from '../aws-exports';
 
 export default function RootLayout({ children }) {
   // useEffect(() => {

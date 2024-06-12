fd3c96d2016396a5be364f1650affeaa8bfc416b
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-12 13:47:47 +0900

    add

--- app/about/page.js
+++ app/about/page.js
@@ -8,6 +8,7 @@ export default function envtest() {
       <p>user_pool: {process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID}</p>
       <p>identity_pool: {process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID}</p>
       <p>user_pool_client_id: {process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID}</p>
+      <p>api_url: {process.env.NEXT_PUBLIC_API_URL}</p>
     </div>
   );
 }
--- aws-exports.js
+++ aws-exports.js
@@ -5,6 +5,7 @@ const awsconfig = {
 	"aws_user_pools_id": process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
 	"aws_user_pools_web_client_id": process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID,
 	"aws_cognito_identity_pool_id": process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID,
-	"aws_cognito_region": process.env.NEXT_PUBLIC_AWS_PROJECT_REGION
+	"aws_cognito_region": process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
+	"aws_api_gateway_url": process.env.NEXT_PUBLIC_API_URL,
 }
 export default awsconfig;

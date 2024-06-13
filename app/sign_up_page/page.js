be36f30a46745d433203ad30630c961f8ac385ec
Changhyun Lee <changhyu@c2r19s1.42gyeongsan.kr>
2024-06-13 15:59:00 +0900

    add

--- app/sign_up_page/page.js
+++ app/sign_up_page/page.js
@@ -1,6 +1,5 @@
 'use client'
 
-
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { Amplify } from 'aws-amplify';
@@ -10,13 +9,13 @@ Amplify.configure(awsconfig);
 
 export default function SignUpPage() {
   return (
-    <Authenticator signUpAttributes={[]}>
-      {({ signOut, user }) => (
+    <Authenticator>
+      {({ signOut }) => (
         <main>
           <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
     </Authenticator>
-  );
+  )
 }

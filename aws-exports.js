cf2d31c16b2643eea3ac7fc87813e0ae62b3690b
Changhyun Lee <changhyu@c3r15s4.42gyeongsan.kr>
2024-07-05 12:19:40 +0900

    add

--- aws-exports.js
+++ aws-exports.js
@@ -1,5 +1,5 @@
 const awsconfig = {
-	// "aws_project_region": process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
+	"aws_project_region": process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
 	// "aws_user_files_s3_bucket": process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET,
 	// "aws_user_files_s3_bucket_region": process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
 	// "aws_user_pools_id": process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
